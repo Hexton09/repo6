356c640217c6cadb004650035a9fa4be340148ea
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-04-20 12:35:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database1.ci9cl96hqxwx.ap-south-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "bloodreaper12345"
+customdb = "employee"
+custombucket = "empbuk"
+customregion = "ap-south-1"
